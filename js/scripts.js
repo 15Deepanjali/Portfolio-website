93cd51160585cb63db7df6d6dd1ebe15e326877a
15Deepanjali <deepanjali.prajak09@gmail.com>
2024-06-21 15:23:37 -0400

    Add responsive mobile menu with jQuery toggle and smooth scrolling

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
 console.log ("Hi! Welcome to my Portfolio Site");
+
+$(document).ready(function() {
+    // Toggle mobile menu
+    $('.menu-icon').click(function() {
+        $('.nav-list').toggleClass('open');
+    });
+
+    // Smooth scrolling
+    $('.nav-item a').on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+});
