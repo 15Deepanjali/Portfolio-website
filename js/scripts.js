9d482866dd2de3dd940ee44d4696a10055888dea
15Deepanjali <deepanjali.prajak09@gmail.com>
2024-07-01 16:46:57 -0400

    made changes in CSS&HTML
    -made changes almost in everything alignment to colors to animation width  .

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,10 @@
 console.log ("Hi! Welcome to my Portfolio Site");
 
-$(document).ready(function() {
-    // Toggle mobile menu
-    $('.menu-icon').click(function() {
-        $('.nav-list').toggleClass('open');
-    });
-
-    // Smooth scrolling
-    $('.nav-item a').on('click', function(event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            var hash = this.hash;
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function(){
-                window.location.hash = hash;
-            });
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+        if (x.className === 'navtoggle'){
+            x.className += ' responsive';
+        }else{
+            x.className = 'navtoggle';
         }
-    });
-});
+}
